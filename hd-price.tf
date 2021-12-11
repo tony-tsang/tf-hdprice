11bc622101bb2c693a5b2a97f11c4f01b5c20ab1
Tony Tsang <tcmtsang@gmail.com>
2021-12-11 13:48:07 +0800

    fix key reference

--- hd-price.tf
+++ hd-price.tf
@@ -15,7 +15,7 @@ data "aws_kms_key" "hd_price_timestream_kms_key" {
 
 resource "aws_timestreamwrite_database" "hd_price_database" {
     database_name = "hd-price"
-    kms_key_id = aws_kms_key.hd_price_timestream_kms_key.arn
+    kms_key_id = data.aws_kms_key.hd_price_timestream_kms_key.arn
     tags = {
         Name = "hd-price"
     }
@@ -70,7 +70,7 @@ resource "aws_lambda_function" "fetch_price" {
 resource "aws_cloudwatch_log_group" "hd_price_log_group" {
     name = "/aws/lambda/hd-price"
     retention_in_days = "30"
-    # kms_key_id = aws_kms_key.hd_price_timestream_kms_key.arn
+    kms_key_id = data.aws_kms_key.hd_price_timestream_kms_key.arn
 }
 
 data "aws_iam_user" "robot_user" {
